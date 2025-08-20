9736beb77bc934491789524076a7d1e578a416ee
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-08-19 22:48:44 -0300

    Commit do projeto

---
+++ src/test/java/br/com/fiap/fiapautenticacao/FiapAutenticacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiapautenticacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapAutenticacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
