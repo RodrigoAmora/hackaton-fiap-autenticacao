152cbe37ca3c4420b53f9df1b300707b9aef3860
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-08-19 22:53:23 -0300

    Commit do projeto

---
+++ src/main/java/br/com/fiap/fiapautenticacao/FiapAutenticacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiapautenticacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapAutenticacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiapAutenticacaoApplication.class, args);
+    }
+
+}
