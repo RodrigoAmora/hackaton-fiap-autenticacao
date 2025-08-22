9ad09c0fa06e0a890c4dbe91de6e4620faa00698
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-08-22 10:04:05 -0300

    Shellscript para rodar o MongoDB

---
+++ run_mongo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+##################################
+## Shellscript to start MongoDB ##
+##################################
+
+
+echo -e "\033[01;32m########################\033[01;32m"
+echo -e "\033[01;32m### Starting MongoDB ###\033[01;32m"
+echo -e "\033[01;32m########################\033[01;32m"
+echo -e "\n\n"
+
+source ~/.bash_profile
+
+cd $MONGO_HOME/bin
+sudo ./mongod --dbpath /usr/local/var/mongodb
