98abee79cdd8c63cbfe99bb4b129ea887e18926f
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-08-27 23:41:07 -0300

    Endpoints buscar todos os usuários e pegar o usuário logado

--- src/main/java/br/com/fiap/fiapautenticacao/controller/api/UsuarioController.java
+++ src/main/java/br/com/fiap/fiapautenticacao/controller/api/UsuarioController.java
@@ -4,11 +4,11 @@ import br.com.fiap.fiapautenticacao.controller.api.doc.UsuarioControllerDoc;
 import br.com.fiap.fiapautenticacao.dto.UsuarioDTO;
 import br.com.fiap.fiapautenticacao.dto.request.UsuarioRequest;
 import br.com.fiap.fiapautenticacao.service.UsuarioService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/usuario")
@@ -20,8 +20,24 @@ public class UsuarioController implements UsuarioControllerDoc {
         this.usuarioService = usuarioService;
     }
 
+    @Override
     @PostMapping
     public ResponseEntity<UsuarioDTO> create(@RequestBody UsuarioRequest request) {
         return ResponseEntity.ok(usuarioService.salvarUsuario(request));
     }
+
+    @Override
+    @GetMapping("/me")
+    public UsuarioDTO getUsuarioLogado(Authentication authentication) {
+        return usuarioService.buscarUsuarioPorEmail(authentication.getName());
+    }
+
+    @Override
+    @GetMapping
+    @PreAuthorize("hasRole('MODERATOR') || hasRole('ADMIN')")
+    public ResponseEntity<Page<UsuarioDTO>> buscarUsuarios(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
+                                                           @RequestParam(value = "size", required = false, defaultValue = "20") int size) {
+        return ResponseEntity.ok(usuarioService.buscarUsuarios(page, size));
+    }
+
 }
