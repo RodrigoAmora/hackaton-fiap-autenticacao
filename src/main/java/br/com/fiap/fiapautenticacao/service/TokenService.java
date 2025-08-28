5f0591ca08fccf1e3e7adfca5ab440dbc9d70446
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-08-27 23:44:11 -0300

    Pegando o role do usuário

--- src/main/java/br/com/fiap/fiapautenticacao/service/TokenService.java
+++ src/main/java/br/com/fiap/fiapautenticacao/service/TokenService.java
@@ -1,13 +1,22 @@
 package br.com.fiap.fiapautenticacao.service;
 
+import br.com.fiap.fiapautenticacao.model.Usuario;
+import br.com.fiap.fiapautenticacao.repository.UsuarioRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TokenService {
@@ -15,13 +24,36 @@ public class TokenService {
     @Value("${jwt.secret}")
     private String secretKey;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     public String generateToken(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         Date now = new Date();
         Date expiration = new Date(now.getTime() + 86400000); // 24 horas
 
+        Usuario usuario = usuarioRepository.findByEmail(user.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        String role = switch (usuario.getRole().getName()) {
+            case ROLE_USER -> "USER";
+            case ROLE_ADMIN -> "ADMIN";
+            case ROLE_MODERATOR -> "MODERATOR";
+            default -> "USER";
+        };
+
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(
+                new SimpleGrantedAuthority("ROLE_" + role)
+        );
+
+        List<String> roles = authorities.stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+
         return Jwts.builder()
                 .setSubject(user.getUsername())
+                .claim("roles", roles)
                 .setIssuedAt(new Date())
                 //.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1h
                 .setExpiration(expiration)
