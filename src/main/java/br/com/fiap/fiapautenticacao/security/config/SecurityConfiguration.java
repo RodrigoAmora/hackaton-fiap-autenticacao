4628770c979f2e1a57bb4d94074b15b3c3d33963
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-08-27 23:42:01 -0300

    Adicionando a anotação EnableMethodSecurity

--- src/main/java/br/com/fiap/fiapautenticacao/security/config/SecurityConfiguration.java
+++ src/main/java/br/com/fiap/fiapautenticacao/security/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -18,6 +19,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true
+)
 public class SecurityConfiguration {
 
     @Autowired
@@ -31,8 +37,8 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers("/api/auth/**").permitAll();
                     req.requestMatchers("/actuator/**").permitAll();
-                    req.requestMatchers(HttpMethod.POST, "/api/usuario").permitAll();
 
+                    req.requestMatchers(HttpMethod.POST, "/api/usuario").permitAll();
 
                     req.anyRequest().authenticated();
                 })
